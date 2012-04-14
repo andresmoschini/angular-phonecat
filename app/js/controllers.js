7b30a3b157a162df7fe02ae8cdcc8b37771fd2a5
Andres Moschini <andresmoschini@gmail.com>
2012-04-14 09:29:19 -0300

    Follow step 3

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,13 @@
 /* App Controllers */
 
 function PhoneListCtrl() {
-    this.hello = "Hello, World!";
+    var ctrl = this;
+    var count = 0;
+    this.hello = function () {
+        console.debug(this);
+        return "Hello, World! (" + count++ + ")";
+    }
+    this.query = "Motor";
     this.phones = [
         { "name": "Nexus S", "snippet": "Fast just got faster with Nexus S." },
         { "name": "Motorola XOOM™ with Wi-Fi", "snippet": "The Next, Next Generation tablet." },
