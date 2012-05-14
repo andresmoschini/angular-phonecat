64561fb3d4b0fff4b26aa7f85c0d5777f30a2d3f
Andres Moschini <andresmoschini@gmail.com>
2012-05-14 15:27:41 -0300

    step-4 Two-way Data Binding

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -29,5 +29,17 @@ describe('PhoneCat App', function () {
             //alternative version of the last assertion that tests just the value of the binding
             using('#status').expect(binding('query')).toBe('nexus');
         });
+
+        it('should be possible to control phone order via the drop down select box', function () {
+            input('query').enter('tablet'); //let's narrow the dataset to make the test assertions shorter
+
+            expect(repeater('.phones li', 'Phone List').column('phone.name')).
+                toEqual(["Motorola XOOM\u2122 with Wi-Fi", "MOTOROLA XOOM\u2122"]);
+
+            select('orderProp').option('Alphabetical');
+
+            expect(repeater('.phones li', 'Phone List').column('phone.name')).
+                toEqual(["MOTOROLA XOOM\u2122", "Motorola XOOM\u2122 with Wi-Fi"]);
+        });
     });
 });
