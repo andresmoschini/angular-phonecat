d22e6e49bc3b1944acf8df1e09c0d57f70271aa7
Andres Moschini <andresmoschini@gmail.com>
2012-05-14 15:43:47 -0300

    step-5 XHRs & Dependency Injection

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -5,11 +5,30 @@ describe('PhoneCat controllers', function () {
 
     describe('PhoneListCtrl', function () {
 
-        it('should create "phones" model with 3 phones', function () {
-            var scope = {};
-            var ctrl = new PhoneListCtrl(scope); 
+        var scope, ctrl, $httpBackend;
 
-            expect(scope.phones.length).toBe(3);
+        beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('phones/phones.json').
+                respond([{ name: 'Nexus S' }, { name: 'Motorola DROID'}]);
+            scope = $rootScope.$new();
+            ctrl = $controller(PhoneListCtrl, { $scope: scope });
+        }));
+
+        it('should create "phones" model with 2 phones fetched from xhr', function() {
+            expect(scope.phones).toBeUndefined();
+            $httpBackend.flush();
+            expect(scope.phones)
+                .toEqual([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+        });
+
+        it('should create "phones" model with 2 phones', function () {
+            $httpBackend.flush();
+            expect(scope.phones.length).toBe(2);
+        });
+
+        it('should set the default value of orderProp model', function () {
+            expect(scope.orderProp).toBe('age');
         });
     });
 });
