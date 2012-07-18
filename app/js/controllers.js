a8280a4eac3c4d277dd7445ba1b6d68cfb8727f9
Andres Moschini <andresmoschini@gmail.com>
2012-07-18 09:21:29 -0300

    some fun with locations

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,9 +2,35 @@
 
 /* Controllers */
 
-function PhoneListCtrl($scope, Phone) {
+function PhoneListCtrl($scope, $routeParams, $location, $browser, Phone) {
     $scope.phones = Phone.query();
     $scope.orderProp = 'age';
+    //$scope.testField = $routeParams.testParameter;
+    /*
+    $scope.$watch(function () { return $location.hash() }, function (value) {
+        $scope.testParameter = value;
+    });
+
+    $scope.$watch('testParameter', function (value) {
+        $location.hash(value).replace();
+    });
+    */
+
+    $scope.$watch(function () { return $location.search().testParameter }, function (value) {
+        $scope.testParameter = value;
+    });
+
+    $scope.$watch('testParameter', function (value) {
+        //$location.search({ testParameter: value }).replace();
+        //$browser.url($location.absUrl(), $location.$$replace);
+        $location.search({ testParameter: value }).preventLocationChangedEvent();
+    });
+    
+    /*
+    $scope.testChange = function () {
+        $location.search({ testParameter: $scope.testField });
+    };
+    */
 }
 
 //PhoneListCtrl.$inject = ['$scope', 'Phone'];
